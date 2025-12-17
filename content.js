b2c3a34948fa5fb408285b247cd57960079a10cb
jjgallagher-ship-it <jj.gallagher@preface.ai>
2025-12-17 13:04:42 +0800

    #Goldman's ScratchJr Portfolio

---
+++ content.js
@@ -0,0 +1,27 @@
+// CONFIGURATION
+// Parents/Teachers: Edit the information inside the quotes below.
+
+const portfolioConfig = {
+    // 1. Student Info
+    password: "123", // The password to unlock the site
+    pageTitle: "Goldman's ScratchJr Portfolio",
+    studentName: "Goldman",
+    studentBio: "Hi! I am 5 years old and I love creating stories about space and dinosaurs. I learned to use loops and messages in ScratchJr to make my characters come to life!",
+    
+    // 2. Projects List
+    // To add a new project, copy one section between { } and paste it below.
+    projects: [
+        {
+            title: "Space Adventure",
+            description: "I used the speed block to make the rocket go fast. The alien sends a message to the astronaut to start dancing.",
+            thumbnail: "", // The filename of the screenshot in the images folder
+            videoLink: "" // The link to the video on Google Drive
+        },
+        {
+            title: "The Dancing Cat",
+            description: "My first project! I learned how to make the cat jump and spin when I tap him.",
+            thumbnail: "",
+            videoLink: ""
+        }
+    ]
+};
