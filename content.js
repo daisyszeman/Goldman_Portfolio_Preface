c401bfcf8d6f6e1b8587a50991bb9de2ddcbdfd8
daisyszeman <98395928+daisyszeman@users.noreply.github.com>
2026-02-01 14:25:43 +0800

    Added 5 projects
    added 5 projects with thumbnail, description and videos

--- content.js
+++ content.js
@@ -12,16 +12,35 @@ const portfolioConfig = {
     // To add a new project, copy one section between { } and paste it below.
     projects: [
         {
-            title: "Space Adventure",
-            description: "I used the speed block to make the rocket go fast. The alien sends a message to the astronaut to start dancing.",
-            thumbnail: "", // The filename of the screenshot in the images folder
-            videoLink: "" // The link to the video on Google Drive
+            title: "Angry Bird",
+            description: "This is a flappy angry bird game. I added buttons to move the bird up and down and put obstacles in the way. If the bird hits an obstacle, the game is over. If the bird stays safe for 10 seconds, you win!🐥",
+            thumbnail: "thumbnail/angrybird.jpg", // The filename of the screenshot in the images folder
+            videoLink: "https://drive.google.com/file/d/1iMqa4dM68KmF9l-Zv8H6k3AyHg1d9-Zv/view?usp=sharing" // The link to the video on Google Drive
         },
         {
-            title: "The Dancing Cat",
-            description: "My first project! I learned how to make the cat jump and spin when I tap him.",
-            thumbnail: "",
-            videoLink: ""
+            title: "Car Racing",
+            description: "I made a fast racing game with cool futuristic cars! This is my first time generating AI pictures for my game! The car moves left and right to dodge traffic. If there is a crash, an alert sign pops up. The game ends—and the race is over! Let’s start the race now! 🏁",
+            thumbnail: "thumbnail/carRacing.jpg",
+            videoLink: "https://drive.google.com/file/d/1TSxPTrbjDtwPcYnA_31hb_hkYv3c86M0/view?usp=sharing"
+        },
+                {
+            title: "Matching Game",
+            description: "I made a fun matching game with cards and letters! At the start, the card shows the letter for a short time, then hides again. Remember the letters carefully! Drag the letters to match the cards. Match them right and celebrate!🃏",
+            thumbnail: "thumbnail/matchingGame.jpg",
+            videoLink: "https://drive.google.com/file/d/12rO3lc3nsnOMFZlSJs6g_UBhD6TYmkz0/view?usp=sharing"
+        },
+                {
+            title: "Treasure Hunt",
+            description: "Come and find the treasure! Use the arrows to move the character. If you touch the square man, you go back to start! Catch the car to jump to the river level. Finally grab the red line and get the treasure! 🏆",
+            thumbnail: "thumbnail/TreasureHunt.jpg",
+            videoLink: "https://drive.google.com/file/d/11I06wnXQr4RC9q8IzLP_6CRyquK4IamB/view?usp=sharing"
+        },
+                {
+            title: "Maze Game",
+            description: "I built a maze game! I drew the maze walls, place the character and treasure in the maze. Use the arrow buttons to move the character. If you touch the wall, you go back to start. Reach the end, catch the treasure, and you win! 🎉",
+            thumbnail: "thumbnail/mazeGame.jpg",
+            videoLink: "https://drive.google.com/file/d/1iwUZ6wJzMOVPtFxB5QPQy-gfcqM7digM/view?usp=sharing"
         }
+
     ]
 };
